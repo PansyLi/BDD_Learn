4a3fc4a6e40e7d0893b00e73aaaa32e2cb277497
PansyLi <pansy9122@foxmail.com>
2019-04-05 16:50:53 +0800

    update

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -1,11 +1,16 @@
 package Pages;
 
+import config.TestConfig;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class LoginPage extends Page{
     private static final Logger log = LoggerFactory.getLogger(LoginPage.class);
@@ -18,16 +23,31 @@ public class LoginPage extends Page{
     @FindBy(id="dologin")
     private WebElement login_button;
 
+    @FindBy(id = "spnUid")
+    private WebElement userid;
+
+    @FindBy(xpath = ".//*[@id='_mail_component_32_32']/span[2]")
+    private WebElement inbox;
+
     private WebDriverWait wait = new WebDriverWait(driver, 15);
 
     public LoginPage(WebDriver shareDriver) {
         super(shareDriver);
     }
-
-    public void test(){
-        driver.get("https://mail.163.com/");
-
+    public void login163Mail() throws IOException, InterruptedException {
+        driver.get(TestConfig.valueFor("app.url"));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".header-163logo")));
+        username.sendKeys(TestConfig.valueFor("username"));
+        wait(3000);
+        password.sendKeys(TestConfig.valueFor("password"));
+        wait(3000);
+        login_button.click();
     }
-
-
+    public void loginAssert(){
+        if (inbox.isDisplayed()==true){
+            log.info("login 163 mail successfully!\n");
+        } else {
+            log.error("Login Failed :-(\n");
+        }
+    }
 }
--- src/test/stepDefinition/testSteps/SendEmailStepdefs.java
+++ src/test/stepDefinition/testSteps/SendEmailStepdefs.java
@@ -1,22 +1,27 @@
 package testSteps;
 
+import Pages.LoginPage;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class SendEmailStepdefs {
+    public LoginPage loginPage;
+    public SendEmailStepdefs(LoginPage commonloginPage){
+        this.loginPage = commonloginPage;
+
+    }
     @Given("^Customer login (\\d+) mail successfully$")
     public void customer_login_mail_successfully(int arg1) throws Exception {
         // Write code here that turns the phrase above into concrete actions
-
+        loginPage.login163Mail();
+        loginPage.loginAssert();
     }
 
     @When("^Customer write an email and click send button$")
     public void customer_write_an_email_and_click_send_button() throws Exception {
         // Write code here that turns the phrase above into concrete actions
-
     }
-
     @Then("^Email send successfully$")
     public void email_send_successfully() throws Exception {
         // Write code here that turns the phrase above into concrete actions
